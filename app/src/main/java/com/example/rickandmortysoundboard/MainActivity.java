14259250da79362b41b14c71c04d0ea909487a93
Kaitlyn <kaitie.98@hotmail.com>
2019-12-19 18:24:43 +1000

    Added more sounds

--- app/src/main/java/com/example/rickandmortysoundboard/MainActivity.java
+++ app/src/main/java/com/example/rickandmortysoundboard/MainActivity.java
@@ -1,38 +1,45 @@
 package com.example.rickandmortysoundboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import models.ButtonResource;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static MediaPlayer mp = null;
+    private static MediaPlayer mp;
+
+    private ButtonResource[] sounds = {
+            new ButtonResource(R.id.lickMyBalls, "lick_my_balls.mp3"),
+            new ButtonResource(R.id.wubbaLubbaDubDub, "woo_vu_luvub_dub_dub.wav"),
+            new ButtonResource(R.id.getSchwifty, "get_schwifty_in_here.wav"),
+            new ButtonResource(R.id.mrPoopybutthole, "mr_poopybutwhole.mp3"),
+            new ButtonResource(R.id.riggityWrecked, "riggity_wrecked.wav"),
+            new ButtonResource(R.id.tinyRick, "tiny_rick.wav")
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playLickMyBalls = this.findViewById(R.id.lickMyBalls);
-        Button playWubbaLubbaDubDub = this.findViewById(R.id.wubbaLubbaDubDub);
-
-        playLickMyBalls.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play(getApplicationContext(), R.raw.lick_my_balls);
-            }
-        });
-
-        playWubbaLubbaDubDub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play(getApplicationContext(), R.raw.woo_vu_luvub_dub_dub);
-            }
-        });
+        for (ButtonResource sound : sounds) {
+            final String resource = sound.getResource();
+            Button soundButton = this.findViewById(sound.getId());
+            soundButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    play(getApplicationContext(), resource);
+                }
+            });
+        }
     }
 
     /**
@@ -40,13 +47,21 @@ public class MainActivity extends AppCompatActivity {
      * @param context context of the media player
      * @param resource sound resource
      */
-    public static void play(Context context, int resource)
+    public void play(Context context, String resource)
     {
+        // stop any media before trying to play another one
         stop(context);
+        AssetFileDescriptor afd;
+        try {
+            mp = new MediaPlayer();
+            afd = getAssets().openFd(resource);
+            mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            mp.prepare();
+            mp.start();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
-        mp = MediaPlayer.create(context, resource);
-        mp.setLooping(false);
-        mp.start();
     }
 
     /**
--- app/src/main/java/models/ButtonResource.java
+++ app/src/main/java/models/ButtonResource.java
@@ -1,6 +1,24 @@
 package models;
 
 public class ButtonResource {
-    public static final int id = 0;
-    public static final int resource = 0;
+    private int id;
+    private String resource;
+
+    public ButtonResource() {
+        this.id = 0;
+        this.resource = "";
+    }
+
+    public ButtonResource(int id, String resource) {
+        this.id = id;
+        this.resource = resource;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getResource() {
+        return this.resource;
+    }
 }
