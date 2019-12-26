b73c57d62da307f749e6a64a91bd5e4bc6687b46
Kaitlyn <kaitie.98@hotmail.com>
2019-12-26 18:51:30 +1000

    Updated sound

--- app/src/main/java/com/example/rickandmortysoundboard/MainActivity.java
+++ app/src/main/java/com/example/rickandmortysoundboard/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             new ButtonResource(R.id.lickMyBalls, "lick_my_balls.mp3"),
             new ButtonResource(R.id.wubbaLubbaDubDub, "woo_vu_luvub_dub_dub.wav"),
             new ButtonResource(R.id.getSchwifty, "get_schwifty_in_here.wav"),
-            new ButtonResource(R.id.mrPoopybutthole, "mr_poopybutwhole.mp3"),
+            new ButtonResource(R.id.mrPoopybutthole, "mr_poopybutthole.mp3"),
             new ButtonResource(R.id.riggityWrecked, "riggity_wrecked.wav"),
             new ButtonResource(R.id.tinyRick, "tiny_rick.wav")
     };
